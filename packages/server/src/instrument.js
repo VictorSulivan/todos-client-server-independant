37408ffc88e3810ed42bdb11967a51bb388ac091
Victor Sulivan <matheoprofessionnel12@gmail.com>
2025-11-05 14:52:22 +0100

    chore: config sentry

---
+++ packages/server/src/instrument.js
@@ -0,0 +1,11 @@
+import * as Sentry from '@sentry/node';
+
+Sentry.init({
+  dsn: process.env.SENTRY_DSN,
+  environment: process.env.NODE_ENV || 'development',
+  sendDefaultPii: true,
+});
+
+export default Sentry;
+
+
